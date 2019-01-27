c9d57141184b55afa9a669195fa4267bbb342731
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2019-01-27 20:43:13 +0100

    Fix TestDrive to use the new Op.

--- src/test/java/fr/pasteur/iah/localzprojector/TestDrive.java
+++ src/test/java/fr/pasteur/iah/localzprojector/TestDrive.java
@@ -6,23 +6,16 @@ import java.util.Locale;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-import fr.pasteur.iah.localzprojector.process.LocalProjectionParameters;
-import fr.pasteur.iah.localzprojector.process.LocalProjectionParameters.ProjectionMethod;
-import fr.pasteur.iah.localzprojector.process.LocalProjectionOp;
-import fr.pasteur.iah.localzprojector.process.ReferenceSurfaceOp;
+import fr.pasteur.iah.localzprojector.process.ExtractSurfaceParameters;
+import fr.pasteur.iah.localzprojector.process.ExtractSurfaceParameters.ProjectionMethod;
+import fr.pasteur.iah.localzprojector.process.LocalZProjectionOp;
 import fr.pasteur.iah.localzprojector.process.ReferenceSurfaceParameters;
 import fr.pasteur.iah.localzprojector.process.ReferenceSurfaceParameters.Method;
 import net.imagej.Dataset;
 import net.imagej.ImageJ;
-import net.imagej.ImgPlus;
-import net.imagej.axis.Axes;
 import net.imagej.ops.special.function.Functions;
-import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.img.Img;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
-import net.imglib2.type.numeric.integer.IntType;
-import net.imglib2.view.Views;
 
 public class TestDrive
 {
@@ -30,17 +23,19 @@ public class TestDrive
 	public static < T extends RealType< T > & NativeType< T > > void main( final String[] args ) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException, IOException
 	{
 		final String imageFile = "samples/Composite.tif";
+		final boolean showReferencePlane = true;
+		final boolean showOutputDuringCalculation = true;
 		final int channel = 1;
-		final ReferenceSurfaceParameters params = ReferenceSurfaceParameters.create()
+		final ReferenceSurfaceParameters referenceSurfaceParameters = ReferenceSurfaceParameters.create()
 				.method( Method.MAX_OF_STD )
 				.zMin( 0 )
 				.zMax( 100000 )
 				.halfWindowSize( 10 )
 				.gaussianPreFilter( 1. )
 				.medianPostFilterHalfSize( 20 )
+				.targetChannel( channel )
 				.get();
-
-		final LocalProjectionParameters params2 = LocalProjectionParameters.create()
+		final ExtractSurfaceParameters extractSurfaceParameters = ExtractSurfaceParameters.create()
 				.zOffset( 0, +8 )
 				.zOffset( 1, 0 )
 				.deltaZ( 0, 3 )
@@ -59,31 +54,16 @@ public class TestDrive
 		ij.ui().show( open );
 
 		final Dataset dataset = ij.dataset().getDatasets().get( 0 );
-		@SuppressWarnings( "unchecked" )
-		final Img< T > img = ( Img< T > ) dataset.getImgPlus().getImg();
-
-		// Extract reference.
-
-		final int channelAxis = dataset.dimensionIndex( Axes.CHANNEL );
-		final RandomAccessibleInterval< T > source;
-		if ( channelAxis >= 0 )
-			source = Views.hyperSlice( img, channelAxis, channel );
-		else
-			source = img;
-
 		@SuppressWarnings( { "rawtypes", "unchecked" } )
-		final ReferenceSurfaceOp< T > op = ( ReferenceSurfaceOp ) Functions.unary( ij.op(), ReferenceSurfaceOp.class, Img.class, source, params );
-		final Img< IntType > referenceSurface = op.calculate( source );
-
-		ij.ui().show( referenceSurface );
-
-		// Extract surface.
-
-		@SuppressWarnings( { "unchecked", "rawtypes" } )
-		final LocalProjectionOp< T > op2 = ( LocalProjectionOp ) Functions.binary( ij.op(), LocalProjectionOp.class, Dataset.class, dataset, referenceSurface, params2 );
-		@SuppressWarnings( "unchecked" )
-		final ImgPlus< T > surface = op2.calculate( ( ImgPlus< T > ) dataset.getImgPlus(), referenceSurface );
-
-		ij.ui().show( surface );
+		final LocalZProjectionOp< T > localZProjectionOp = ( LocalZProjectionOp ) Functions.unary(
+				ij.op(),
+				LocalZProjectionOp.class,
+				Dataset.class,
+				Dataset.class,
+				referenceSurfaceParameters,
+				extractSurfaceParameters,
+				showReferencePlane,
+				showOutputDuringCalculation );
+		localZProjectionOp.calculate( dataset );
 	}
 }
